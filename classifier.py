912ef0f62ac33a360a14f2925b11b03bc60e4ed7
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-20 20:11:53 -0800

    Pixels of 255 where successfully converted to 1

--- classifier.py
+++ classifier.py
@@ -1,4 +1,21 @@
 from taipy.gui import Gui
+from tensorflow.keras import models
+from PIL import Image
+import numpy as np
+
+model = models.load_model("baseline_mariya.keras")
+def predict_image(model, path_to_img):
+    #print(model.summary())
+    #print(path_to_img)
+    img = Image.open(path_to_img)
+    img = img.convert("RGB")
+    img = img.resize((32,32))
+    data = np.asarray(img)  #normalizing the images
+    print("before", data[0][0])
+    data = data / 255 
+    print("after", data[0][0])
+
+    probs = model.predict(data) #check if image is compatible with the network
 
 content = ""
 
@@ -20,7 +37,8 @@ select an image from your file system
 def on_change(state, var_name, var_val):
     if var_name == "content":
         state.img_path = var_val
-    print(var_name, var_val)
+        predict_image(model, var_val)
+    #print(var_name, var_val)
 
 app = Gui(page=index)
 
